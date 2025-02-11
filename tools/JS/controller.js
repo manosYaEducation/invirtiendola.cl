c3aed2e6d51b4550e6a59b2b9aac9a5673a8ff2c
naxolate <naxolate22@gmail.com>
2025-02-10 22:56:21 -0300

    MVC Ok

---
+++ tools/JS/controller.js
@@ -0,0 +1,3 @@
+//trae los datos del html, las envia al modelo
+
+//recive nuevos datos del modelo y los envia a View
--- tools/MVCcontroller.js
+++
@@ -1,3 +0,0 @@
-//trae los datos del html, las envia al modelo
-
-//recive nuevos datos del controllador y los envia a View
